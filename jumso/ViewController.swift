fe8ee0b5863ca825abc007bddfb6b1f413ce761a
JunhaPark96 <njk116@gmail.com>
2024-09-19 00:24:03 +0900

    Initial Commit

---
+++ jumso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jumso
+//
+//  Created by junha on 9/19/24.
+// test
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
