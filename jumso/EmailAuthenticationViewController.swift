d7c8f1cda8b55db8d31d149fc6b8be2171e629d5
JunhaPark96 <njk116@gmail.com>
2024-09-29 02:47:45 +0900

    [FEAT] 회원가입 스토리보드 작성 중. 이메일 인증 페이지 구현 중

---
+++ jumso/EmailAuthenticationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmailAuthenticationViewController.swift
+//  jumso
+//
+//  Created by junha on 9/29/24.
+//
+
+import UIKit
+
+class EmailAuthenticationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ jumso/ProfileCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileCell.swift
+//  jumso
+//
+//  Created by junha on 9/29/24.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
