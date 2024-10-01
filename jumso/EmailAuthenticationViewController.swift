9f94da0fe2cdabfeb03eb7abace74edad80a76a6
JunhaPark96 <njk116@gmail.com>
2024-10-02 00:15:44 +0900

    [Bug Fix] Email Authentication View Controller 키보드 UI문제 해결. 다시 키보드를 내릴 때 # emailAuthVC.buttonBottomConstraint.constant = 50 가 문제 였음. 주석 처리하니 화면 전환도 가능. 단, 버튼이 이동하고, 다시 돌아올 방법을 찾아야함

--- jumso/EmailAuthenticationViewController.swift
+++ jumso/EmailAuthenticationViewController.swift
@@ -7,8 +7,8 @@ class EmailAuthenticationViewController: UIViewController {
     @IBOutlet weak var InputEmailIDTextField: UITextField!
     @IBOutlet weak var EmailAddressLabel: UILabel!
     @IBOutlet weak var emailAuthenticationButton: UIButton!
+
     @IBOutlet weak var buttonBottomConstraint: NSLayoutConstraint!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +17,7 @@ class EmailAuthenticationViewController: UIViewController {
             setupKeyBoardDismissal()
         }
     }
+    
     deinit {
         if shouldManageKeyboardObservers {
             removeKeyboardNotificationObservers()
@@ -24,19 +25,12 @@ class EmailAuthenticationViewController: UIViewController {
     }
     
     @IBAction func emailAuthenticateDidTap(_ sender: UIButton) {
-        // 키보드 내리기 -> 키보드가 안내려가면 화면전환이 안됨
         dismissKeyboard()
         
-        // 1. 스토리보드 생성
         let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
-        
-        // 2. 뷰 컨트롤러 생성
         let emailAuthenticationViewController = storyboard.instantiateViewController(withIdentifier: "AuthenticationCodeVC") as! AuthenticationCodeViewController
         
-        // 3. 화면전환 메소드로 화면 전환
         self.navigationController?.pushViewController(emailAuthenticationViewController, animated: true)
-        
     }
-    
-
 }
+
--- jumso/ViewController.swift
+++ jumso/ViewController.swift
@@ -1,14 +1,32 @@
 import UIKit
 
+class ViewController: UIViewController {
+    var shouldManageKeyboardObservers: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if shouldManageKeyboardObservers {
+            setupKeyBoardDismissal()
+        }
+    }
+
+    deinit {
+        if shouldManageKeyboardObservers {
+            removeKeyboardNotificationObservers()
+        }
+    }
+}
+
 extension UIViewController {
     func setupKeyBoardDismissal() {
-        // 키보드 외 화면 탭 시 키보드 내리기
+        // 화면 탭 시 키보드 내리기
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGesture.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tapGesture)
         
+        // 키보드 이벤트 옵저버 등록
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
@@ -16,33 +34,29 @@ extension UIViewController {
         self.view.endEditing(true)
     }
     
-    
-    
-    @objc func keyboardWillShow(notification: NSNotification){
+    @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardHeight = keyboardFrame.cgRectValue.height
             
-            // 버튼의 제약 조건을 키보드 위로 이동
+            // EmailAuthenticationViewController에서 buttonBottomConstraint만 변경
             if let emailAuthVC = self as? EmailAuthenticationViewController {
                 UIView.animate(withDuration: 0.3) {
-                    emailAuthVC.buttonBottomConstraint.constant = keyboardHeight + 5  // 키보드 위로 20pt 여유
-                    emailAuthVC.view.layoutIfNeeded()  // 레이아웃 업데이트
+                    emailAuthVC.buttonBottomConstraint.constant = keyboardHeight + 10 // 키보드 바로 위로 10pt 여유
+                    emailAuthVC.view.layoutIfNeeded() // 레이아웃 업데이트
                 }
             }
         }
     }
     
-    @objc func keyboardWillHide(notification: NSNotification){
-        // 키보드가 내려가면 버튼의 하단 제약을 원래 위치로 복원
+    @objc func keyboardWillHide(notification: NSNotification) {
         if let emailAuthVC = self as? EmailAuthenticationViewController {
             UIView.animate(withDuration: 0.3) {
-                emailAuthVC.buttonBottomConstraint.constant = 50  // 원래 위치로 복원 (필요시 마진 조정)
-                emailAuthVC.view.layoutIfNeeded()  // 레이아웃 업데이트
+//                emailAuthVC.buttonBottomConstraint.constant = 50 // 원래 위치로 복원
+                emailAuthVC.view.layoutIfNeeded() // 레이아웃 업데이트
             }
         }
     }
-    
-    // 옵저버 해제
+
     func removeKeyboardNotificationObservers() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
