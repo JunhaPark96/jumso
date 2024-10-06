1332d83682aa0fdbc218e0a7d7c880cf6aadc3de
JunhaPark96 <njk116@gmail.com>
2024-10-06 22:57:07 +0900

    [Add] SignUpGenderViewController, SignUpDistanceViewController UI 레이아웃 설정

---
+++ jumso/SignUp/Cell/SignUpProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpProfileTableViewCell.swift
+//  jumso
+//
+//  Created by junha on 10/6/24.
+//
+
+import UIKit
+
+class SignUpProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
