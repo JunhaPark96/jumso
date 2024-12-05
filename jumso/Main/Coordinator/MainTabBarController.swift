88eaab6b6778830bf8caeade846ecad6af0d1d9b
JunhaPark96 <njk116@gmail.com>
2024-12-05 19:12:24 +0900

    [Chore] MainTabBarController: ChatViewController 방식 변경

--- jumso/Main/Coordinator/MainTabBarController.swift
+++ jumso/Main/Coordinator/MainTabBarController.swift
@@ -15,22 +15,22 @@ class MainTabBarController: UITabBarController {
         homeVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
         
         let loungeVC = LoungeViewController()
-        let chatVC = ChatViewController()
+        
+        let chatView = ChatView()
+        let chatVC = UIHostingController(rootView: chatView)
+        chatVC.tabBarItem = UITabBarItem(title: "채팅", image: UIImage(systemName: "message"), selectedImage: UIImage(systemName: "message.fill"))
+        
+        
         let profileVC = ProfileViewController()
         
-//        homeVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
         loungeVC.tabBarItem = UITabBarItem(title: "라운지", image: UIImage(systemName: "bubble.left.and.bubble.right"), selectedImage: UIImage(systemName: "bubble.left.and.bubble.right.fill"))
-        chatVC.tabBarItem = UITabBarItem(title: "채팅", image: UIImage(systemName: "message"), selectedImage: UIImage(systemName: "message.fill"))
         profileVC.tabBarItem = UITabBarItem(title: "프로필", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
         
         
-        
-//        let homeNav = UINavigationController(rootViewController: homeVC)
         let loungeNav = UINavigationController(rootViewController: loungeVC)
-        let chatNav = UINavigationController(rootViewController: chatVC)
         let personNav = UINavigationController(rootViewController: profileVC)
         
-        self.viewControllers = [homeVC, loungeNav, chatNav, personNav]
+        self.viewControllers = [homeVC, loungeNav, chatVC, personNav]
         
         self.selectedIndex = 0
     }
